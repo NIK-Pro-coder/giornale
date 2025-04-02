c19a2213401d10327d5d250db909eca753f86180
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2025-04-02 16:34:59 +0200

    First Commit

---
+++ scripts/backend.py
@@ -0,0 +1,93 @@
+import sys
+import json
+import time
+import os
+import requests
+import json
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+WORLD_NEWS_API_KEY = os.getenv("WORLD_NEWS_API_KEY")
+
+def getWorldNews(endpoint: str) :
+	link = f"https://api.worldnewsapi.com/{endpoint}"
+
+	r = requests.get(
+		link,
+		headers={
+			"x-api-key": WORLD_NEWS_API_KEY
+		}
+	)
+
+	if not r.ok :
+		return {
+			"success": False,
+			"code": r.status_code,
+			"reason": r.reason
+		}
+
+	got = json.loads(r.text)
+	got["success"] = True
+
+	return got
+
+def getNews(articles_num: int = 100) :
+	news = []
+
+	left = articles_num
+	offset = 0
+
+	while left > 0 :
+		info = getWorldNews(f"search-news?language=en&number={min(100, left)}&offset={offset}")
+
+		if not info["success"] :
+			return info
+
+		if not "news" in info :
+			return []
+
+		news.extend(info["news"])
+
+		left -= info["number"]
+		offset += info["number"]
+
+	return news
+
+cmdict = {}
+
+#Decorator to automatically rgister
+#functions as callable with the cli
+def addEntry(func) :
+	cmdict[func.__name__] = func
+	return func
+
+@addEntry
+def news() -> dict :
+	currentday = int(time.time() / 60 / 60 / 24)
+
+	try :
+		with open(f"cache/{currentday}.json") as f :
+			return {
+				"cached": True,
+				"news": json.load(f)
+			}
+	except :
+		news = getNews(1000)
+
+		if type(news) is dict :
+			return news
+
+		with open(f"cache/{currentday}.json", "w") as f :
+			json.dump(news, f)
+
+		return {
+			"cached": False,
+			"news": news
+		}
+
+if __name__ == "__main__" :
+	d: dict = cmdict[sys.argv[1]](*sys.argv[2:])
+
+	print(json.dumps(d))
---
+++ scripts/index.js
@@ -0,0 +1,105 @@
+function updateFilter() {
+	const input = document.getElementById("category");
+	const news = document.getElementById("news");
+
+	document.getElementById("content").value = "";
+
+	let art = false;
+
+	for (let i = 0; i < news.children.length; i++) {
+		let me = news.children[i];
+		console.log(me.children);
+		let category = me.children[3].children[0].innerHTML;
+
+		if (category.startsWith(input.value)) {
+			me.hidden = false;
+			art = true;
+		} else {
+			me.hidden = true;
+		}
+	}
+
+	document.getElementById("noart").hidden = art;
+}
+
+function filterContent() {
+	const input = document.getElementById("content");
+	const news = document.getElementById("news");
+
+	document.getElementById("category").value = "";
+
+	let art = false;
+
+	for (let i = 0; i < news.children.length; i++) {
+		let me = news.children[i];
+		console.log(me.children);
+		let category = me.children[2].innerHTML;
+
+		if (category.toLowerCase().includes(input.value.toLowerCase())) {
+			me.hidden = false;
+			art = true;
+		} else {
+			me.hidden = true;
+		}
+	}
+
+	document.getElementById("noart").hidden = art;
+}
+
+async function main() {
+	const cache = document.getElementById("cache");
+	cache.hidden = true;
+	document.getElementById("noart").hidden = true;
+
+	let given = await fetch("/news");
+	let data = await given.json();
+
+	document.getElementById("load").hidden = true;
+
+	const news_div = document.getElementById("news");
+
+	if (data.success == false) {
+		news_div.innerHTML = `Cannot get news articles, reason: "${data.reason}"`;
+		return;
+	}
+
+	cache.hidden = false;
+	if (data.cached) {
+		cache.innerHTML = "Using cached news articles";
+	}
+
+	const NEWS = data.news;
+
+	for (let i = 0; i < NEWS.length; i++) {
+		const n = NEWS[i];
+
+		const details = document.createElement("details");
+		details.innerHTML = `<blockquote><p>Summary:</p>${n.summary}</blockquote>`;
+
+		details.innerHTML += `<a href="${n.url}">Article link</a>`;
+
+		details.innerHTML += `<p>${n.text}</p>`;
+
+		const summary = document.createElement("summary");
+
+		if (n.category !== undefined) {
+			summary.innerHTML += `<code>${n.category}</code>`;
+		} else {
+			summary.innerHTML += '<code class="hide">no category</code>';
+		}
+
+		summary.innerHTML += `<h3>${n.title}</h3>`;
+
+		details.appendChild(summary);
+
+		news_div.appendChild(details);
+	}
+
+	const input = document.getElementById("category");
+	input.addEventListener("input", updateFilter);
+	updateFilter();
+
+	const content = document.getElementById("content");
+	content.addEventListener("input", filterContent);
+	filterContent();
+}
